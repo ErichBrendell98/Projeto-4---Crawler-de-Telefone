6012dbeff4fb0e5e62bcc5f1ab8b62c5f8e557bd
Erich Brendell <erychbrendell@gmail.com>
2022-09-27 22:09:24 -0300

    added: descobrir_telefones() & __main__

--- crawler.py
+++ crawler.py
@@ -1,4 +1,5 @@
 import re
+import threading
 
 import requests
 from bs4 import BeautifulSoup
@@ -8,7 +9,11 @@ DOMINIO = 'https://django-anuncios.solyd.com.br'
 URL_AUTOMOVEIS = 'https://django-anuncios.solyd.com.br/automoveis/'
 
 
-def requisição(url):
+LINKS = []
+TELEFONES = []
+
+
+def requisicao(url):
     try:
         resposta = requests.get(url)
         if resposta.status_code == 200:
@@ -58,3 +63,44 @@ def encontrar_telefone(soup):
     regex = re.findall(r"\(?0?([1-9]{2})[ \-\.\)]{0,2}(9[ \-\.]?\d{4})[ \-\.]?(\d{4})", descricao)
     if regex:
         return regex
+
+
+def descobrir_telefones():
+    while True:
+        try:
+            link_anuncio = LINKS.pop(0)
+        except:
+            return None
+
+        resposta_anuncio = requisicao(DOMINIO + link_anuncio)
+
+        if resposta_anuncio:
+            soup_anuncio = parsing(resposta_anuncio)
+            if soup_anuncio:
+                telefones = encontrar_telefone(soup_anuncio)
+                if telefones:
+                    for telefone in telefones:
+                        print(f'Telefone encontrado {telefone}')
+                        TELEFONES.append(telefone)
+
+
+if __name__ == "__main__":
+    resposta_busca = requisicao(URL_AUTOMOVEIS)
+    if resposta_busca:
+        soup_busca = parsing(resposta_busca)
+        if soup_busca:
+            LINKS = encontrar_links(soup_busca)
+
+            thread_1 = threading.Thread(target=descobrir_telefones)
+            thread_2 = threading.Thread(target=descobrir_telefones)
+            thread_3 = threading.Thread(target=descobrir_telefones)
+
+            thread_1.start()
+            thread_2.start()
+            thread_3.start()
+
+            thread_1.join()
+            thread_2.join()
+            thread_3.join()
+
+            print(TELEFONES)
